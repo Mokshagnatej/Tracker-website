f33211473e933e316f4f278d9e6a88e7ee182859
Mokshag <mokshagnatejkalepalli@gmail.com>
2026-09-09 21:39:48 +0530

    feat: configure API proxy and add concurrently scripts to run UI and backend in dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,11 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 8443,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
   },
 })
