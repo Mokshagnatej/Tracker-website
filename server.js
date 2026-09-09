4b756a21edf996209d0c287c33977edf7ae195c6
Mokshag <mokshagnatejkalepalli@gmail.com>
2026-09-09 22:25:14 +0530

    fix: express 5 path-to-regexp error

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.all('/api/metadata', (req, res) => require('./api/metadata.js')(req, res));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
